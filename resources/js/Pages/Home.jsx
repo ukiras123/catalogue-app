3f95834106658fb82d89eb853f5979827e398cde
Kiran Gautam <ukiras@gmail.com>
2024-09-18 15:54:58 +1000

    added title to the page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Grid2 as Grid, Card, CardContent, Typography, CardMedia, CardHeader, Container } from '@mui/material';
 import CategoryCard from '../Components/CategoryCard';
 import CategoryLayout from '../Layouts/CategoryLayout';
+import { Head } from '@inertiajs/react';
 
 export default function Home({ content }) {
     return (
         <CategoryLayout title={"Browse Categories"}>
+            <Head title="Home" />
             <Grid container spacing={{ xs: 2, md: 3 }} justifyContent={"center"}>
                 {content.map((item) => (
                     <Grid size={{ xs: 8, sm: 6, md: 4, lg: 3, xl: 2 }} key={item.id}>
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
+const appName = import.meta.env.VITE_APP_NAME || 'Catalogue';
 
 createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
     resolve: (name) => {
         const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
         return pages[`./Pages/${name}.jsx`]
